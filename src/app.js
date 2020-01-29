424dfe63426e76efaa64390110a3c0fb17c71b9e
Sterling Watts <SterlingDWatts@gmail.com>
2020-01-29 14:51:09 -0800

    Add winston for logging. Create cards and lists arrays

---
+++ src/app.js
@@ -0,0 +1,57 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const winston = require("winston");
+const { NODE_ENV } = require("./config");
+
+// create Express app
+const app = express();
+
+// log 'tiny' output if in production, else log 'common'
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
+app.use(morgan(morganOption));
+
+// log all "info" events with winston
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  transports: [new winston.transports.File({ filename: "info.log" })]
+});
+
+if (NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple()
+    })
+  );
+}
+
+// hide sensitive data with 'helmet' and allow cors
+app.use(helmet());
+app.use(cors());
+
+//
+const cards = [{ id: 1, title: "Task One", content: "This is card one" }];
+const lists = [{ id: 1, header: "List One", cardIds: [1] }];
+
+// basic endpoint for app.js
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// error handling middleware gives short response if in production
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === "production") {
+    response = { error: { message: "server error" } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
+
+// export the app
+module.exports = app;
