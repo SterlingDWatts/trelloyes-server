4efacfc7d775a9a806052fed278b2ac4972b33a3
Sterling Watts <SterlingDWatts@gmail.com>
2020-01-30 12:29:40 -0800

    Create DELETE /list/:id and /card/:id endpoints

--- src/app.js
+++ src/app.js
@@ -77,6 +77,7 @@ app.get("/card/:id", (req, res) => {
   res.json(card);
 });
 
+// POST /card endpoint
 app.post("/card", (req, res) => {
   const { title, content } = req.body;
 
@@ -107,6 +108,31 @@ app.post("/card", (req, res) => {
     .json(card);
 });
 
+// DELETE /card endpoint
+app.delete("/card/:id", (req, res) => {
+  const { id } = req.params;
+
+  const cardIndex = cards.findIndex(c => c.id == id);
+
+  if (cardIndex === -1) {
+    logger.error(`Card with id ${id} not found.`);
+    return res.status(404).send("Not Found");
+  }
+
+  // remove card from lists
+  // assume cardIds are not duplicated in the cardIds array
+  lists.forEach(list => {
+    const cardIds = list.cardIds.filter(cid => cid !== id);
+    list.cardIds = cardIds;
+  });
+
+  cards.splice(cardIndex, 1);
+
+  logger.info(`Card with id ${id} deleted.`);
+
+  res.status(204).end();
+});
+
 // GET /list endpoint returns array of lists
 app.get("/list", (req, res) => {
   res.json(lists);
@@ -126,7 +152,7 @@ app.get("/list/id", (req, res) => {
   res.json(list);
 });
 
-// POST /card endpoint
+// POST /list endpoint
 app.post("/list", (req, res) => {
   const { header, cardIds = [] } = req.body;
 
@@ -168,6 +194,22 @@ app.post("/list", (req, res) => {
     .status(201)
     .location(`http://localhost:8000/list/${id}`)
     .json({ id });
+});
+
+app.delete("/list/:id", (req, res) => {
+  const { id } = req.params;
+
+  const listIndex = lists.findIndex(li => li.id == id);
+
+  if (listIndex === -1) {
+    logger.error(`List with id ${id} not found.`);
+    return res.status(404).send("Not Found");
+  }
+
+  lists.splice(listIndex, 1);
+
+  logger.info(`List with id ${id} deleted.`);
+  res.status(204).end();
 });
 
 // error handling middleware gives short response if in production
